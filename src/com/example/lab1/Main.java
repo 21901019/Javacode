b3911f2d459f450ec3945d26ef88534552f530af
21901019 <akqlshrl2002@naver.com>
2021-09-05 22:45:37 +0900

    2nd commit

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -3,10 +3,14 @@ package com.example.lab1;
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Hello World!!!\n");
-		System.out.print("이름 입력 : ");
-		String name = s.next();
-		System.out.println("["+name+"[님 환영합니다~");
+	    Scanner s = new Scanner(System.in);
+	    System.out.print("Enter a length of rectangle > ");
+	    int length = s.nextInt();
+	    for(int i = 0; i < length; i++){
+	      for (int j = 0; j < length; j++){
+	        System.out.print("# ");
+	      }
+	      System.out.println("");
+	    }
 	}
 }
